d96782eb0f027d99195a231b2d2372909a29424f
majd idani <majd.idani@esprit.tn>
2022-09-15 18:43:43 +0100

    eee3

---
+++ Controllers/ThemeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestApiJWT.Models;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ThemeController : ControllerBase
+    { 
+        private readonly ApplicationDbContext _context;
+        public ThemeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/Theme
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<theme>>> GetTheme()
+        {
+            return await _context.theme.ToListAsync();
+        }
+
+
+    }
+
+}
